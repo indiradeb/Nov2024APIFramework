ab6894b0d4488653eb62536aae2f2eb251224efc
indiradeb <160302225+indiradeb@users.noreply.github.com>
2025-02-23 23:41:29 -0800

    Update GetUserTest.java

--- src/test/java/com/qa/gorest/tests/GetUserTest.java
+++ src/test/java/com/qa/gorest/tests/GetUserTest.java
@@ -44,7 +44,7 @@ public class GetUserTest extends BaseTest{
 		List<String> userEmail = js.readList(gorestResponse, "$..[3].email");
 		System.out.println(userEmail);
 		Assert.assertTrue(userEmail.contains("adripathi_tagore@greenholt-mertz.test"));
-		
+		//add more assertions
 	}
 	
 
